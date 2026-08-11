7ff8efb9b197c88a2dab0613fc937347e06d90cd
Mike Manzano <mike@clicketyclacks.co>
2026-08-11 08:14:01 +0000

    feat: add durable harness health incidents

--- lib/tightbeam/schema.ex
+++ lib/tightbeam/schema.ex
@@ -28,7 +28,8 @@ defmodule Tightbeam.Schema do
     Tightbeam.Supervision,
     Tightbeam.WorkState,
     Tightbeam.Productions.BubbleSweeper,
-    Tightbeam.HarnessProcess
+    Tightbeam.HarnessProcess,
+    Tightbeam.HarnessHealth
   ]
 
   # The shape this build writes. Bump it when a production table changes in a
