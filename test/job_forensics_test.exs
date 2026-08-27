81b5af9259643ad6f6398f29517d2ee720a555d1
Mike Manzano <mike@clicketyclacks.co>
2026-08-27 00:44:43 +0000

    Accept body fields in work-item detail proof

--- test/job_forensics_test.exs
+++ test/job_forensics_test.exs
@@ -192,8 +192,8 @@ defmodule Tightbeam.JobForensicsTest do
     # migrate the item.
     assert_keys(
       fetch_item(db, "wi_disp").workItem,
-      ~w(createdAt createdBySession createdByUser failReason id isBug ownerUserId
-         specRefName specRefSha256 state title)a
+      ~w(body bodyUpdatedAt bodyUpdatedBySession bodyUpdatedByUser createdAt createdBySession
+         createdByUser failReason id isBug ownerUserId specRefName specRefSha256 state title)a
     )
   end
 
